9d2fa0e8ab4c32015594d95c03387a30c0a66ce2
SirRendore <gravitymail2723@gmail.com>
2020-08-13 18:03:48 -0400

    remove caster damping

--- controller_recorder/csv_ang_vel_publisher.py
+++ controller_recorder/csv_ang_vel_publisher.py
@@ -14,16 +14,16 @@ SCALING = 1
 
 
 rospy.init_node('csv_ang_vel_publisher')
-pub_left = rospy.Publisher("joint_velocity_controller_left/command", Float64, queue_size = 10)
-pub_right = rospy.Publisher("joint_velocity_controller_right/command", Float64, queue_size = 10)
+pub_left = rospy.Publisher("WheelL_velocity_controller/command", Float64, queue_size = 10)
+pub_right = rospy.Publisher("WheelR_velocity_controller/command", Float64, queue_size = 10)
 
 finished = rospy.Publisher("finished_sim", Bool, queue_size = None)
 started = rospy.Publisher("started_sim", Bool, queue_size = None)
 
 # Reset simulation
-rospy.wait_for_service('/gazebo/reset_simulation')
-reset_simulation = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
-reset_simulation()
+# rospy.wait_for_service('/gazebo/reset_simulation')
+# reset_simulation = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+# reset_simulation()
 
 # Stop Wheelchair
 pub_left.publish(0)
